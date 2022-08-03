81d3865af07fc5ff62369f075431ae8ad669bcbe
md-sahil-11 <mdsahilalam11@gmail.com>
2022-08-03 15:29:14 +0530

    apis integration

---
+++ src/components/ajax/hooks/useApi.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+export default function useApi() {
+  const baseURL = "http://127.0.0.1:8000/";
+
+  const instance = axios.create({
+    baseURL: baseURL,
+    headers: {
+      Authorization: localStorage.getItem("access_token")
+        ? "Token " + localStorage.getItem("access_token")
+        : null,
+      "Content-Type": "application/json",
+      accept: "application/json",
+    },
+  });
+
+  return instance
+}
