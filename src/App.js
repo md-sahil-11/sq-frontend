778e0f5d3cf7b89888f11d6fb537a33f975cb016
AM-SAPP <8923499556nikhil@gmail.com>
2022-08-08 01:58:28 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ function App() {
   return (
     <div className="App">
       <Switch>
+        <Route path="/" exact component={WorkSpace}/>
         <Route path="/sign-up" exact component={SignUp} />
         <Route path="/sign-in" exact component={SignIn} />
         <Route path="/casestudies" exact component={CaseStudies} />
